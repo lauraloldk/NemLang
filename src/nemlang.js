6173b78c7f745ff34c52e2be71608ea0fe636f63
laura F. Nielsen <mikkellarsen500@gmail.com>
2023-04-12 20:42:15 +0200

    Update nemlang.js

--- src/nemlang.js
+++ src/nemlang.js
@@ -55,45 +55,47 @@
     }
     };
 
+    NemLang.dot = function(callback, showInput) {
+    function createDotAndExecuteCallback(event) {
+        var x = event.clientX;
+        var y = event.clientY;
 
-    NemLang.dot = function (callback, showInput) {
-        function createDotAndExecuteCallback(event) {
-            var x = event.clientX;
-            var y = event.clientY;
+        // Create a black dot at the clicked position
+        var dot = document.createElement("div");
+        dot.className = "nemlang-dot";
+        dot.style.position = "absolute";
+        dot.style.left = x + "px";
+        dot.style.top = y + "px";
+        dot.style.width = "5px";
+        dot.style.height = "5px";
+        dot.style.backgroundColor = "black";
+        document.body.appendChild(dot);
 
-            // Create a black dot at the clicked position
-            var dot = document.createElement("div");
-            dot.style.position = "absolute";
-            dot.style.left = x + "px";
-            dot.style.top = y + "px";
-            dot.style.width = "5px";
-            dot.style.height = "5px";
-            dot.style.backgroundColor = "black";
-            document.body.appendChild(dot);
-
-            // Show input box with coordinates if showInput is true
-            if (showInput) {
-                var textBox = document.createElement("input");
-                textBox.type = "text";
-                textBox.value = `x: ${x}, y: ${y}`;
-                textBox.style.position = "absolute";
-                textBox.style.left = x + 10 + "px";
-                textBox.style.top = y + 10 + "px";
-                document.body.appendChild(textBox);
-            }
-
-            // Remove the click event listener after one click
-            document.removeEventListener("click", createDotAndExecuteCallback);
-
-            // Execute the callback function with the coordinates
-            if (typeof callback === "function") {
-                callback(x, y);
-            }
+        // Show input box with coordinates if showInput is true
+        if (showInput) {
+            var textBox = document.createElement("input");
+            textBox.type = "text";
+            textBox.className = "nemlang-dot-input";
+            textBox.value = `x: ${x}, y: ${y}`;
+            textBox.style.position = "absolute";
+            textBox.style.left = (x + 10) + "px";
+            textBox.style.top = (y + 10) + "px";
+            document.body.appendChild(textBox);
         }
 
-        // Add the click event listener
-        document.addEventListener("click", createDotAndExecuteCallback);
+        // Remove the click event listener after one click
+        document.removeEventListener("click", createDotAndExecuteCallback);
+
+        // Execute the callback function with the coordinates
+        if (typeof callback === "function") {
+            callback(x, y);
+        }
+    }
+
+    // Add the click event listener
+    document.addEventListener("click", createDotAndExecuteCallback);
     };
+
 
     NemLang.SpawnObject = function (x, y, element) {
         element.style.position = "absolute";
@@ -147,8 +149,18 @@
     });
     };
 
+    NemLang.Cleardot = function() {
+    var dots = document.querySelectorAll(".nemlang-dot");
+    var inputs = document.querySelectorAll(".nemlang-dot-input");
 
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].remove();
+    }
 
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].remove();
+    }
+    };
 
     // Export NemLang to global scope
     if (typeof module !== "undefined" && module.exports) {
