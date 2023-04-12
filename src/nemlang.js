aeb8fd64ee133d39c93dc6228b38e89eb1b6eca1
laura F. Nielsen <mikkellarsen500@gmail.com>
2023-04-12 02:00:53 +0200

    Added SpawnObject Method

--- src/nemlang.js
+++ src/nemlang.js
@@ -1,67 +1,75 @@
-(function() {
-    'use strict';
+(function () {
+    "use strict";
 
     var NemLang = {};
 
     // A utility function to add two numbers
-    NemLang.plus = function(a, b) {
+    NemLang.plus = function (a, b) {
         return a + b;
     };
 
     // A function to add a paragraph to the site with text and math
-    NemLang.AddP = function(htmlContent) {
-    var p = document.createElement('p');
-    p.innerHTML = htmlContent;
-    document.body.appendChild(p);
+    NemLang.AddP = function (text, skipAppend) {
+        var p = document.createElement("p");
+        p.innerHTML = text;
+        if (!skipAppend) {
+            document.body.appendChild(p);
+        }
+        return p;
     };
 
-	
-	NemLang.color = function(text, color) {
-    return '<span style="color: ' + color + ';">' + text + '</span>';
+    NemLang.color = function (text, color) {
+        return '<span style="color: ' + color + ';">' + text + "</span>";
     };
-   
-    NemLang.dot = function(callback, showInput) {
-    function createDotAndExecuteCallback(event) {
-        var x = event.clientX;
-        var y = event.clientY;
 
-        // Create a black dot at the clicked position
-        var dot = document.createElement("div");
-        dot.style.position = "absolute";
-        dot.style.left = x + "px";
-        dot.style.top = y + "px";
-        dot.style.width = "5px";
-        dot.style.height = "5px";
-        dot.style.backgroundColor = "black";
-        document.body.appendChild(dot);
+    NemLang.dot = function (callback, showInput) {
+        function createDotAndExecuteCallback(event) {
+            var x = event.clientX;
+            var y = event.clientY;
 
-        // Show input box with coordinates if showInput is true
-        if (showInput) {
-            var textBox = document.createElement("input");
-            textBox.type = "text";
-            textBox.value = `x: ${x}, y: ${y}`;
-            textBox.style.position = "absolute";
-            textBox.style.left = (x + 10) + "px";
-            textBox.style.top = (y + 10) + "px";
-            document.body.appendChild(textBox);
+            // Create a black dot at the clicked position
+            var dot = document.createElement("div");
+            dot.style.position = "absolute";
+            dot.style.left = x + "px";
+            dot.style.top = y + "px";
+            dot.style.width = "5px";
+            dot.style.height = "5px";
+            dot.style.backgroundColor = "black";
+            document.body.appendChild(dot);
+
+            // Show input box with coordinates if showInput is true
+            if (showInput) {
+                var textBox = document.createElement("input");
+                textBox.type = "text";
+                textBox.value = `x: ${x}, y: ${y}`;
+                textBox.style.position = "absolute";
+                textBox.style.left = x + 10 + "px";
+                textBox.style.top = y + 10 + "px";
+                document.body.appendChild(textBox);
+            }
+
+            // Remove the click event listener after one click
+            document.removeEventListener("click", createDotAndExecuteCallback);
+
+            // Execute the callback function with the coordinates
+            if (typeof callback === "function") {
+                callback(x, y);
+            }
         }
 
-        // Remove the click event listener after one click
-        document.removeEventListener("click", createDotAndExecuteCallback);
+        // Add the click event listener
+        document.addEventListener("click", createDotAndExecuteCallback);
+    };
 
-        // Execute the callback function with the coordinates
-        if (typeof callback === "function") {
-            callback(x, y);
-        }
-    }
+    NemLang.SpawnObject = function (x, y, element) {
+        element.style.position = "absolute";
+        element.style.left = x + "px";
+        element.style.top = y + "px";
+        document.body.appendChild(element);
+    };
 
-    // Add the click event listener
-    document.addEventListener("click", createDotAndExecuteCallback);
-};
-	
-   
     // Export NemLang to global scope
-    if (typeof module !== 'undefined' && module.exports) {
+    if (typeof module !== "undefined" && module.exports) {
         module.exports = NemLang;
     } else {
         window.NemLang = NemLang;
