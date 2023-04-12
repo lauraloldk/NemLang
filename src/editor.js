e87adddd637ca2af05401d9cee5d5ef227524ba3
laura F. Nielsen <mikkellarsen500@gmail.com>
2023-04-13 00:39:17 +0200

    Update editor.js

--- src/editor.js
+++ src/editor.js
@@ -8,7 +8,8 @@ function deactivateEditorFeatures() {
     // Your editor features deactivation code goes here
 }
 
-function createEditorToolbox() {
+function createEditorToolbox() 
+{
   // Create the toolbox container
   const toolbox = document.createElement('div');
   toolbox.id = 'editor-toolbox';
@@ -20,23 +21,41 @@ function createEditorToolbox() {
   toolbox.style.display = 'none';
 
   // Create buttons and add them to the toolbox
-  for (let i = 1; i <= 2; i++) {
-        const button = document.createElement('button');
-        
-        if (i === 1) {
-            button.textContent = 'Get X,Y';
-            button.onclick = function() {
-                event.stopPropagation();
-				NemLang.dot(0, true);
-            };
-        } else {
-            button.textContent = `Button ${i}`;
-        }
+    for (let i = 1; i <= 2; i++) {
+    const button = document.createElement('button');
 
-        button.style.display = 'block';
-        button.style.marginBottom = '10px';
-        toolbox.appendChild(button);
+    if (i === 1) {
+      button.textContent = 'Get X,Y';
+      button.onclick = function() {
+        event.stopPropagation();
+        NemLang.dot(0, true);
+      };
+    } else if (i === 2) {
+      button.textContent = 'Add P';
+      button.onclick = function(event) {
+        event.stopPropagation();
+
+        NemLang.dot(function(x, y) {
+          NemLang.Cleardot();
+
+          var elementName = prompt('Please enter a name for the paragraph:');
+          var paragraphText = prompt('Please enter the text for the paragraph:');
+          var paragraph = NemLang.AddP(paragraphText || 'This is a paragraph created by Add P button.', true);
+
+          if (elementName) {
+            window[elementName] = paragraph;
+            NemLang.SpawnObject(x, y, paragraph);
+          }
+        }, true);
+      };
+    } else {
+      button.textContent = `Button ${i}`;
     }
+
+    button.style.display = 'block';
+    button.style.marginBottom = '10px';
+    toolbox.appendChild(button);
+  } 
 
   // Add the toolbox to the body
   document.body.appendChild(toolbox);
@@ -45,3 +64,5 @@ function createEditorToolbox() {
 }
 
 const editorToolbox = createEditorToolbox();
+
+
